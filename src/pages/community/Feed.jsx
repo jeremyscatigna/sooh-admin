308fb8315b268d0358d26e1ddac3ffcfb662fd2b
Jeremy Scatigna <scatignaj@me.com>
2023-11-13 15:54:40 +0100

    feat(test): tab bar ui full page

--- src/pages/community/Feed.jsx
+++ src/pages/community/Feed.jsx
@@ -121,15 +121,18 @@ function Feed() {
     return (
         <div className='flex h-screen overflow-hidden'>
             {/* Sidebar */}
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            {mobile && (
+            {mobile ? (
                 <SlidingTabBar />
+            ): (
+                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
             )}
 
             {/* Content area */}
             <div className='relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
                 {/*  Site header */}
-                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                {!mobile && (
+                    <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                )}
 
                 <main>
                     <div className='px-4 sm:px-6 lg:px-8 py-8 md:py-0 w-full max-w-9xl mx-auto'>
