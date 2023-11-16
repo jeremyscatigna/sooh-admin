42e2edebf34eca7f5801f7c04666aa9a175b195d
Jeremy Scatigna <scatignaj@me.com>
2023-11-16 10:57:41 +0100

    feat(eslint): add a proper eslint config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,37 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import postcss from './postcss.config.js';
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  css: {
-    postcss,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: /^~.+/,
-        replacement: (val) => {
-          return val.replace(/^~/, "");
-        },
-      },
+    define: {
+        'process.env': process.env,
+    },
+    css: {
+        postcss,
+    },
+    plugins: [
+        react(),
+        eslintPlugin({
+            cache: false,
+            include: ['./src/**/*.js', './src/**/*.jsx'],
+            exclude: [],
+        }),
     ],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    }
-  } 
-})
+    resolve: {
+        alias: [
+            {
+                find: /^~.+/,
+                replacement: (val) => {
+                    return val.replace(/^~/, '');
+                },
+            },
+        ],
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+});
