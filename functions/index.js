f65ddca9e91274d087e979953df5ec469b60a305
Jeremy Scatigna <scatignaj@me.com>
2023-11-29 15:40:16 +0100

    feat(hh): fix function

--- functions/index.js
+++ functions/index.js
@@ -53,7 +53,7 @@ exports.recreateDailyHH = onSchedule("every day 00:00", async (event) => {
   logger.log("Recreate Daily Happy hour");
 });
 
-export const recreateWeeklyHH = onSchedule("every day 00:00", async (event) => {
+exports.recreateWeeklyHH = onSchedule("every day 00:00", async (event) => {
   // Fetch all user details.
   const happyHours = await admin.firestore().collection("happyhours").get();
   const happyHoursData = happyHours.docs.map((doc) => (
