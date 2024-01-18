4663498a423fbf7e89082d23502b2bc64fdb3025
Jeremy Scatigna <scatignaj@me.com>
2024-01-18 16:03:47 +0100

    feat(dashboard): update stats cards

--- src/partials/dashboard/DashboardCardHappyHours.jsx
+++ src/partials/dashboard/DashboardCardHappyHours.jsx
@@ -7,26 +7,11 @@ function DashboardCardHappyHours({ happyHours }) {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-card shadow-lg rounded-xl">
       <div className="px-5 pt-5">
-        <header className="flex justify-between items-start mb-2">
-          {/* Icon */}
-          <img src={Icon} width="32" height="32" alt="Icon 01" />
-          {/* Menu button */}
-          <EditMenu align="right" className="relative inline-flex">
-            <li>
-              <Link className="font-medium text-sm text-primary hover:text-slate-800 flex py-1 px-3" to="#0">Option 1</Link>
-            </li>
-            <li>
-              <Link className="font-medium text-sm text-primary hover:text-slate-800 flex py-1 px-3" to="#0">Option 2</Link>
-            </li>
-            <li>
-              <Link className="font-medium text-sm text-rose-500 hover:text-rose-600 flex py-1 px-3" to="#0">Remove</Link>
-            </li>
-          </EditMenu>
-        </header>
+        
         <h2 className="text-lg font-semibold text-primary mb-2">Happy Hours</h2>
         <div className="text-xs font-semibold text-secondary uppercase mb-1">Nombre d&apos;Happy hours</div>
         <div className="flex items-center justify-center pb-12">
-          <div className="text-8xl font-bold text-primary mr-2">{happyHours}</div>
+          <div className="text-6xl font-bold text-primary mr-2 mt-4">{happyHours}</div>
         </div>
       </div>
     </div>
