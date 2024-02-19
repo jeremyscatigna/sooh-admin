ff00d09aa60472fc8db55b403c12c3ad112e708e
Jeremy Scatigna <scatignaj@me.com>
2024-02-19 09:23:05 +0100

    feat(redirect): change redirect

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Navigate, redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 const ProtectedRoute = ({ children }) => {
     const { user } = useUserAuth();
 
     console.log("Check user in Private: ", user);
     if (!user) {
-        redirect("/signin");
+        return <Navigate to="/signin" />;
     }
     return children;
 };
