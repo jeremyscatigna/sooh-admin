7c1acf1f8cb14163494affe511116b4b568fbbdc
Jeremy Scatigna <scatignaj@me.com>
2024-03-12 14:20:38 +0100

    feat(profile): delete

--- src/partials/community/ProfileBody.jsx
+++ src/partials/community/ProfileBody.jsx
@@ -458,7 +458,7 @@ function ProfileBody({ profileSidebarOpen, setProfileSidebarOpen, setBasicModalO
                             <div className={`grid ${mobile ? 'grid-cols-1' : 'grid-cols-4'} gap-4`}>
                                 {posts.map((item, i) => (
                                     <div key={item.uid} className='bg-card shadow-md rounded-xl p-5'>
-                                        
+                                        {item.userId === connectedUser.uid && (
                                             <div
                                                 onClick={(e) => {
                                                     e.preventDefault();
@@ -470,7 +470,8 @@ function ProfileBody({ profileSidebarOpen, setProfileSidebarOpen, setBasicModalO
                                             >
                                                 <Trash className='w-5 h-5' />
                                             </div>
-                                        
+                                        )}
+
                                         <div
                                             className={`flex flex-col ${
                                                 !mobile && 'items-center justify-center'
